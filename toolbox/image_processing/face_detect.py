023e11a2895bc0bf7bbc8428638cbfa54f0d9a88
Joseph Sutker <joseph.sutker@students.olin.edu>
2015-03-13 20:15:52 -0400

    Completed shocked face and such

--- toolbox/image_processing/face_detect.py
+++ toolbox/image_processing/face_detect.py
@@ -2,3 +2,29 @@
 
 import cv2
 import numpy as np
+
+cap = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier('/home/jsutker/SoftDesSp15/toolbox/image_processing/haarcascade_frontalface_alt.xml')
+kernel = np.ones((21,21),'uint8')
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    faces = face_cascade.detectMultiScale(frame, scaleFactor=1.2, minSize=(20,20))
+    #print str(faces)
+    for (x,y,w,h) in faces:
+        frame[y:y+h,x:x+w,:] = cv2.dilate(frame[y:y+h,x:x+w,:], kernel)
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255))
+        cv2.circle(frame, (x+(w/3),y+(w/3)), w/8, (255,255,255),-w/8)
+        cv2.circle(frame, (x+(2*w/3),y+(w/3)), w/8, (255,255,255),-w/8)
+        cv2.circle(frame, (x+(w/3),y+(w/3)), w/16, (0,0,0),-w/8)
+        cv2.circle(frame, (x+(2*w/3),y+(w/3)), w/16, (0,0,0),-w/8)
+        cv2.circle(frame, (x+(w/2),y+(5*w/6)), w/10, (0,0,0),-w/8)
+
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
--- toolbox/word_frequency_analysis/frequency.py
+++ toolbox/word_frequency_analysis/frequency.py
@@ -9,7 +9,13 @@ def get_word_list(file_name):
 		returns a list of the words used in the book as a list.
 		All words are converted to lower case.
 	"""
-	pass
+	text = open(file_name,'r')
+	Text = text.read()
+	start = string.find(Text," ***")
+	Text = Text[start+4:]
+	end = string.find(Text,"*** ")
+	Text = string.lower(Text[:end])
+	return string.split(Text)
 
 def get_top_n_words(word_list, n):
 	""" Takes a list of words as input and returns a list of the n most frequently
@@ -19,6 +25,13 @@ def get_top_n_words(word_list, n):
 					punctuation
 		n: the number of words to return
 		returns: a list of n most frequently occurring words ordered from most
-				 frequently to least frequentlyoccurring
+				 frequently to least frequently occurring
 	"""
-	pass
+	wcList = {}
+	for w in word_list:
+		if w not in wcList:
+			wcList[w] = word_list.count(w)
+	ordered_by_frequency = sorted(wcList, key=wcList.get, reverse=True)
+	return ordered_by_frequency[:n]
+
+print get_top_n_words(get_word_list("pg32325.txt"),100)
